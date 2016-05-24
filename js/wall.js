2025dc712902fcea1794628d2dac1c45f825c424
Bastien-Wink <bastien@wink-dev.com>
2016-05-24 12:07:29 +0200

    Directory name change

---
+++ js/wall.js
@@ -0,0 +1,92 @@
+/*
+
+This script be improved by removing ellement once they pass the float-line instead of on-arrive
+Otherwise we skip activities with might have not been seen yet (bellow float line)
+
+*/
+
+function setVisibleAsSeen() {
+
+    chrome.storage.local.get(['activityIds'], function(result) {
+
+        result.activityIds = result.activityIds;
+        if (result.activityIds instanceof Array == false)
+            result.activityIds = new Array;
+
+        $("._5jmm:not(.seenSet)").filter(":onScreen").each(function() {
+            console.log("Set as seen : " + $(this).attr("data-dedupekey"));
+
+            //Mark the activity as seen
+            result.activityIds.unshift($(this).attr("data-dedupekey"));
+
+            // console.log(result.activityIds.length + " activities saved : " + result.activityIds);
+
+            $(this).addClass("seenSet");
+        });
+
+        //Only keep recent elements to save storage
+        if (result.activityIds.length >= 500) {
+            result.activityIds.length = 500;
+        }
+
+        chrome.storage.local.set({
+            'activityIds': result.activityIds
+        });
+    });
+
+}
+
+function hideIfSeen(element, activityIds, hideSeen) {
+    //Not sure why some elements don't have a key, let's skip them
+    if (element.attr("data-dedupekey") == undefined)
+        return;
+
+    if ($.inArray(element.attr("data-dedupekey"), activityIds) > -1) {
+        console.log(element.attr("data-dedupekey") + " Already seen, hiding it");
+
+        countHidden = countHidden + 1;
+        chrome.runtime.sendMessage({
+            type: "setCount",
+            count: countHidden
+        });
+
+        if (hideSeen == true) {
+            element.css("display", "none");
+        }
+    } else {
+        console.log("Not seen" + element.attr("data-dedupekey"))
+    }
+}
+
+var countHidden = 0;
+chrome.runtime.sendMessage({
+    type: "setCount",
+    count: countHidden
+});
+//Retreive activityIds from localed storage
+chrome.storage.local.get(['activityIds', 'hideSeen'], function(result) {
+
+    result.activityIds = result.activityIds;
+    if (result.activityIds instanceof Array == false)
+        result.activityIds = new Array;
+
+    console.log("let's hide these : " + result.activityIds);
+
+    $(document).on("scroll", function() {
+        setVisibleAsSeen();
+    });
+
+
+    $('._5jmm').each(function() {
+        hideIfSeen($(this), result.activityIds, result.hideSeen);
+    });
+
+    $('body').arrive('._5jmm', function() {
+        hideIfSeen($(this), result.activityIds, result.hideSeen);
+    });
+});
+
+//Css injection
+var node = document.createElement('style');
+node.innerHTML = ".seenSet ._5pcp::after {content:' - Seen'} ";
+document.body.appendChild(node);
